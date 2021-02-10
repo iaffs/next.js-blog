f0505249f0e66eeaf435883988dc84c5d5c3fb11
iaffs <byiaffs@gmail.com>
2021-02-10 09:13:48 +0100

    added layout component

--- components/layout.js
+++ components/layout.js
@@ -32,8 +32,8 @@ export default function Layout({ children, home }) {
               priority
               src="/images/profileme.png"
               className={utilStyles.borderCircle}
-              height={144}
-              width={144}
+              height={150}
+              width={150}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
